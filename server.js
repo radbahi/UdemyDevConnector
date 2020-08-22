59988536ad2c3d680beb4283fe1460a0fb823d4d
Radouane Bahi <radbahi@gmail.com>
2020-08-22 14:32:35 -0400

    added user validation

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(
+  express.json({
+    extended: false,
+  })
+);
+
 app.get('/', (req, res) => res.send('API running'));
 
 // Define Routes
