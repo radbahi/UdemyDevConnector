ea50d233239509280c3604b5d6005b241fb09670
Radouane Bahi <radbahi@gmail.com>
2020-09-01 17:46:57 -0400

    testting profile validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const auth = require('../../middleware/auth'); // get auth middleware then add as paramater to get route
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const { check, validationResult } = require('express-validator');
 
 // @route  GET api/profile/me
 // @desc   get current user's profile
@@ -23,5 +24,41 @@ router.get('/me', auth, async (req, res) => {
     res.status(500).send('Server error');
   }
 });
+
+// @route  POST api/profile
+// @desc   create or update user profile
+// @access private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status is required').not().isEmpty(),
+      check('skills', 'Skills is required').not().isEmpty(),
+    ],
+  ], // need these middlewares to authorize and to require status and skills
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // check if any errors
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twiiter,
+      instagram,
+      linkedin,
+    } = req.body;
+  }
+);
 
 module.exports = router;
