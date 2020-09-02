28508fe1f2f4caa999bf9aafe206253d7ab44109
Radouane Bahi <radbahi@gmail.com>
2020-09-02 18:13:05 -0400

    added all profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -104,4 +104,40 @@ router.post(
   }
 );
 
+// @route  GET api/profile
+// @desc   get all profiles
+// @access public
+router.get('/', async (req, res) => {
+  // don't need auth middleware for this one
+  try {
+    const profiles = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate('user', ['name', 'avatar']);
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route  GET api/profile/user/:user_id
+// @desc   get profile by user id
+// @access public
+router.get('/user/:user_id', async (req, res) => {
+  // don't need auth middleware for this one
+  try {
+    const profile = await Profile.find().populate('user', ['name', 'avatar']);
+    if (!profile) {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
