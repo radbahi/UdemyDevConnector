17650e114f19fdfc52274fd18f77c07a1ec2e3a4
Radouane Bahi <radbahi@gmail.com>
2020-09-08 19:12:27 -0400

    add profile experience added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -163,4 +163,56 @@ router.delete('/', auth, async (req, res) => {
   }
 });
 
+// @route  PUT api/profile
+// @desc   add profile experience
+// @access private
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required').not().isEmpty(),
+      check('company', 'Company is required').not().isEmpty(),
+      check('from', 'From date is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      to,
+      from,
+      current,
+      description,
+    } = req.body;
+
+    const newExp = {
+      // make an object with the above attributes
+      title,
+      company,
+      location,
+      to,
+      from,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.experience.unshift(newExp); // we use unshift to put most recent experience first
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
 module.exports = router;
