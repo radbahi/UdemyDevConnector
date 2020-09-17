a9c39f5f302a183923b4e1fa5a0bd08ae71e756a
Radouane Bahi <radbahi@gmail.com>
2020-09-17 14:19:26 -0400

    getting started on redux. getting errors on register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import axios from 'axios';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -10,12 +11,29 @@ const Register = () => {
   const { name, email, password, password2 } = formData;
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value }); // not clear why instructor decided to write onChange separately
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
       console.log('Passwords do not match');
     } else {
-      console.log(formData);
+      const newUser = {
+        name,
+        email,
+        password,
+      };
+
+      try {
+        const config = {
+          headers: {
+            'Content-Type': 'applcation/json',
+          },
+        };
+        const body = JSON.stringify(newUser);
+        const res = await axios.post('/api/users', body, config);
+        console.log(res.status);
+      } catch (err) {
+        console.error(err.response.data);
+      }
     }
   };
   return (
