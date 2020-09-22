4aecafba6660a65a89afb7b9d7276f1894a1c468
Radouane Bahi <radbahi@gmail.com>
2020-09-22 11:58:36 -0400

    added alerts, types

---
+++ client/src/reducers/alert.js
@@ -0,0 +1,18 @@
+import { SET_ALERT } from '../actions/types';
+
+import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
+const initialState = [];
+
+export default function (state = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case SET_ALERT:
+      return [...state, payload]; // adds alert payload with current state and returns them together
+    case REMOVE_ALERT:
+      return state.filter((alert) => alert.id !== payload); // returns all alerts except those that matches payload
+    default:
+      return state;
+  }
+}
+
+//https://www.udemy.com/course/mern-stack-front-to-back/learn/lecture/10055322#questions
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,4 +1,6 @@
 import { combineReducers } from 'redux';
+import alert from './alert';
+
 export default combineReducers({});
 
 //https://www.udemy.com/course/mern-stack-front-to-back/learn/lecture/10055314#questions
