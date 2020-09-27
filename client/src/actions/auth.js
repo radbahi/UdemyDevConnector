92e69d8b1e8dcb071c3a70a4792359580a7cc6c7
Radouane Bahi <radbahi@gmail.com>
2020-09-27 10:15:17 -0400

    added authentication reducer

---
+++ client/src/actions/auth.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
+
+//Register user
+export const register = { name, email, password };
+//https://www.udemy.com/course/mern-stack-front-to-back/learn/lecture/14555594#questions
