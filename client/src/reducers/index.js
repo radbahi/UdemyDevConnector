5a92238aa1895590fe344eda6c39c00381440c05
Radouane Bahi <radbahi@gmail.com>
2020-09-27 10:33:06 -0400

    finished register action

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import alert from './alert';
+import auth from './auth';
 
-export default combineReducers({ alert });
+export default combineReducers({ alert, auth });
 
 //https://www.udemy.com/course/mern-stack-front-to-back/learn/lecture/10055314#questions
