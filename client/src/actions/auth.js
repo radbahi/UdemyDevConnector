faefd5e85ce1e9f99e8082aa4761cf540654b0a8
Radouane Bahi <radbahi@gmail.com>
2020-09-29 15:31:19 -0400

    completed logout

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,6 +7,7 @@ import {
   AUTH_ERROR,
   LOGIN_FAIL,
   LOGIN_SUCCESS,
+  LOGOUT,
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -79,6 +80,11 @@ export const login = (email, password) => async (dispatch) => {
       type: LOGIN_FAIL,
     });
   }
+};
+
+//LOGOUT USER / clear profile
+export const logout = () => (dispatch) => {
+  dispatch({ type: LOGOUT });
 };
 
 export default loadUser;
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -6,5 +6,6 @@ export const USER_LOADED = 'USER_LOADED';
 export const AUTH_ERROR = 'AUTH_ERROR';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT = 'LOGOUT';
 
 // https://www.udemy.com/course/mern-stack-front-to-back/learn/lecture/14555612#questions
