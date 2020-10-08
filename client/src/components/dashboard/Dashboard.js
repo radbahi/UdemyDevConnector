b4dd5a027784eb5ba0b345f71c4994644eb18659
Radouane Bahi <radbahi@gmail.com>
2020-10-08 12:08:33 -0400

    experience table added to dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getCurrentProfile } from '../../actions/profile';
 import DashboardActions from './DashboardActions';
+import Experience from './Experience'; //https://www.udemy.com/course/mern-stack-front-to-back/learn/lecture/10055384#questions
 
 const Dashboard = ({
   getCurrentProfile,
@@ -25,6 +26,7 @@ const Dashboard = ({
       {profile !== null ? (
         <Fragment>
           <DashboardActions />
+          <Experience experience={profile.experience}></Experience>
         </Fragment>
       ) : (
         <Fragment>
