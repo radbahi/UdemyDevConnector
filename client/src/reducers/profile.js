2018e85587af4b1e6c18940a0dd7f75f99eeb707
Radouane Bahi <radbahi@gmail.com>
2020-10-14 15:02:41 -0400

    github repos section added. profile security flaw fixed

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -29,7 +29,7 @@ export default function (state = initialState, action) {
         loading: false,
       };
     case PROFILE_ERROR:
-      return { ...state, error: payload, loading: false };
+      return { ...state, error: payload, loading: false, profile: null };
     case CLEAR_PROFILE:
       return { ...state, profile: null, repos: [], loading: false };
     case GET_REPOS:
