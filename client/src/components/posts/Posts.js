a40c80edeed9020de5cc4bcd434c30a8f61383f6
Radouane Bahi <radbahi@gmail.com>
2020-10-16 12:09:26 -0400

    posts components added in posts page

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,12 +3,29 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/post';
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  return <div></div>;
+  // https://www.udemy.com/course/mern-stack-front-to-back/learn/lecture/10055456#questions
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Posts</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Welcome to the community
+      </p>
+      {/* post form https://www.udemy.com/course/mern-stack-front-to-back/learn/lecture/10055456#questions */}
+      <div className='posts'>
+        {posts.map((post) => (
+          <PostItem key={post._id} post={post}></PostItem>
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
